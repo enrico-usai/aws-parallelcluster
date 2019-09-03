8c2450f242826373df9c2435b18f2e77e1b12da9
Enrico Usai <usai@amazon.com>
2019-09-03 16:34:01 +0200

    Add pyyaml requirement
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- cli/setup.py
+++ cli/setup.py
@@ -22,7 +22,14 @@ def readme():
 
 
 VERSION = "2.4.1"
-REQUIRES = ["boto3>=1.9.54", "future>=0.16.0,<=0.17.1", "tabulate>=0.8.2,<=0.8.3", "ipaddress>=1.0.22", "enum34>=1.1.6"]
+REQUIRES = [
+    "boto3>=1.9.54",
+    "future>=0.16.0,<=0.17.1",
+    "tabulate>=0.8.2,<=0.8.3",
+    "ipaddress>=1.0.22",
+    "enum34>=1.1.6",
+    "PyYAML>=5.1.2",
+]
 
 if sys.version_info[:2] == (2, 6):
     # For python2.6 we have to require argparse since it
