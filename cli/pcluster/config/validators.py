e94c75cc9050ab5bc1639e2dab6da161f0e7e3ba
Enrico Usai <usai@amazon.com>
2019-09-03 16:30:16 +0200

    use unicode
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- cli/pcluster/config/validators.py
+++ cli/pcluster/config/validators.py
@@ -533,7 +533,7 @@ def raid_volume_iops_validator(param_key, param_value, pcluster_config):
     raid_iops = float(param_value)
     raid_vol_size = float(pcluster_config.get_section("raid").get_param_value("volume_size"))
     if raid_iops > raid_vol_size * 50:
-         errors.append("IOPS to volume size ratio of {0} is too high; maximum is 50.".format(raid_iops / raid_vol_size))
+        errors.append("IOPS to volume size ratio of {0} is too high; maximum is 50.".format(raid_iops / raid_vol_size))
 
     return errors, warnings
 
